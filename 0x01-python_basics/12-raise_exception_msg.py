9b6e148f6bcde15c9c244776e50611e6907139ab
Stuart <sgkur04@gmail.com>
2018-05-18 17:48:19 +0000

    12-raise_exception_msg.py: initial commit

---
+++ 0x01-python_basics/12-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 12-raise_exception
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    Description - raises a name exception with a message
+    :param message: the text to display when raising the name exception
+    """
+    raise NameError(message)
