21f88d43fd778d5491bc03d72b09f3e626e8f12b
Stuart <sgkur04@gmail.com>
2018-05-25 17:19:09 +0000

    1-print_comb2.py : test

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-Module 1-print_comb2: prints 00 to 99
-"""
-for i in range(100):
-    if i < 99:
-        print('{:02d}, '.format(i), end="")
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end='')
     else:
-        print("{}".format(i))
+        print(99)
