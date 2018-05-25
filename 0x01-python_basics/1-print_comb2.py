2f0f390456426a64495e808655bf626f3c333b5a
Stuart <sgkur04@gmail.com>
2018-05-25 17:29:14 +0000

    change double quotes in end= to single quotes

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -4,6 +4,6 @@ Module 1-print_comb2: prints 00 to 99
 """
 for i in range(0, 100):
     if i != 99:
-        print('{:02d}, '.format(i), end="")
+        print('{:02d}, '.format(i), end='')
     else:
         print(99)
