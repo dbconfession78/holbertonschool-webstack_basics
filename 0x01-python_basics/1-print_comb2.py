8630d384f455259d3cc89e43d1fd01a0863919aa
Stuart <sgkur04@gmail.com>
2018-05-25 17:15:52 +0000

    1-print_comb2.py : use place formatting instead of using manual '0'

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -4,6 +4,6 @@ Module 1-print_comb2: prints 00 to 99
 """
 for i in range(100):
     if i < 99:
-        print('0{}'.format(i) if i < 10 else '{}'.format(i), end=", ")
+        print('{:02d}, '.format(i), end="")
     else:
         print("{}".format(i))
