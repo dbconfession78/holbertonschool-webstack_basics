d87bc9051ad374e2c895fa27b686426f597c7a1b
Stuart <sgkur04@gmail.com>
2018-05-25 17:38:34 +0000

    original but without doc

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:
-        print("{:02d}, ".format(i), end='')
+    if i < 99:
+        print('0{}'.format(i) if i < 10 else '{}'.format(i), end=", ")
     else:
-        print(99)
+        print("{}".format(i))
