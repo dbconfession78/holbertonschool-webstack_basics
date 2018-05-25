496f658605b6d15b8b37ae86d96c0d240c8ad6c4
Stuart <sgkur04@gmail.com>
2018-05-25 17:09:33 +0000

    PASSED: put doc and hash set back in

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Module 0-print_alphabt
+"""
 for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
+    if chr(i) not in {'q', 'e'}:
         print("{}".format(chr(i)), end='')
