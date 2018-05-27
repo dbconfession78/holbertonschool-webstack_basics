974bfd3bc21e014c22f3bc4ca680eb766ac20c41
Stuart <sgkur04@gmail.com>
2018-05-27 00:34:58 +0000

    fix error in NULL head handler

--- 0x01-python_basics/14-singly_linked_list.py
+++ 0x01-python_basics/14-singly_linked_list.py
@@ -1,86 +1,121 @@
 #!/usr/bin/python3
 """
-module containing Node and SinglyLinkdeList classes
+Module 14-singly_linked_list
 """
 
 
 class Node:
     """
-    defines a node in a singly linked list
+    Node: class deinfition for sll node
     """
     def __init__(self, data, next_node=None):
         """
-        node initialization
+        initialization of Node class
         """
-        self.data = data
-        self.next_node = next_node
+        self.__data = data
+        self.__next_node = next_node
 
     @property
     def data(self):
         """
-        getter for node data
+        data: getter
         """
         return self.__data
 
     @data.setter
     def data(self, value):
         """
-        setter for data attribute
-        @value: integer
+        data: setter
         """
-        if value.__class__ is not int:
-            raise TypeError('data must be an integer')
+        if type(value) != int:
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
         """
-        getter for node node_node
+        next_node: getter
         """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
         """
-        setter for next_node attribute
-        @next_node: Node object or None
+        next_node: setter
         """
-        if value is None or type(value) is Node:
-            self.__next_node = value
-        else:
-            raise TypeError('next_node must be a Node object')
+        if type(value) not in {Node, None}:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
 
 
 class SinglyLinkedList:
     """
-    defines a singly linked list
+    SinglyLinkeList: class definition for a singly linked list
     """
+
     def __init__(self):
         """
-        linked list initialization
+        SinglyLinkedList class initialization
         """
         self.__head = None
 
     def __repr__(self):
         """
-        returns the node data in the singly linked list
+        ___repr___ for SinglyLinkedList class
         """
-        node_list = []
-        i = self.__head
-        while i is not None:
-            node_list.append(str(i.data))
-            i = i.next_node
-        return '\n'.join(node_list)
+        return self
+
+    def __str__(self):
+        """
+        ___str___ for SinglyLinkedList class
+        """
+
+        retval = ""
+        walk = self.__head
+        while walk:
+            retval += "{}".format(str(walk.data))
+
+            if walk.next_node is not None:
+                retval += "\n"
+            walk = walk.next_node
+        return "{}".format(retval)
 
     def sorted_insert(self, value):
         """
-        inserts a node into the list in increasing order
+        inserts a node into a sorted sll
+        :value: 'data' value of node being inserted
         """
-        if self.__head is None or self.__head.data >= value:
-            self.__head = Node(value, next_node=self.__head)
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+
+        elif self.__head and self.__head.data >= value:
+            new_node.next_node = self.__head
+            self.__head = new_node
         else:
             walk = self.__head
-            while walk.next_node and walk.next_node.data < value:
+            prev = None
+            while walk:
+                if walk.data >= value:
+                    if prev:
+                        prev.next_node = new_node
+                    else:
+                        self.__head = new_node
+                    new_node.next_node = walk
+                    break
+                if prev is None and walk.data >= value:
+                    self.head = new_node
+                    new_node.next_node = walk
+                    break
+                prev = walk
+                if walk.next_node:
+                    if walk.next_node.data >= value:
+                        new_node.next_node = walk.next_node
+                        walk.next_node = new_node
+                        break
+                    else:
+                        walk = walk.next_node
+                else:
+                    walk.next_node = new_node
+                    break
                 walk = walk.next_node
-            new_node = Node(value, next_node=walk.next_node)
-            walk.next_node = new_node
