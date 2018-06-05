4bd11bae362e954d1961017ad323c3bc4332344a
Stuart <sgkur04@gmail.com>
2018-06-05 20:51:08 +0000

    8-starwars.py: initial commit

---
+++ 0x03-python_web_scraping/8-starwars.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module 8-starwars -  takes in a string and sends a search
+                     request to the Star Wars API 'people' endpoint
+                     (https://swapi.co/documentation)
+"""
+
+
+def main():
+    import requests
+    from sys import argv
+
+    name = argv[1]
+    url = "http://swapi.co/api/people/?search={}".format(name)
+    r = requests.get(url)
+    j = r.json()
+    retval = []
+    if type(j) is dict:
+        if j == {}:
+            print("No result")
+        else:
+            print("Number of result: {}".format(j.get("count")))
+            while j.get('results'):
+                results = j.get('results')
+                for result in results:
+                    retval.append(result.get("name"))
+                if j.get("next"):
+                    r = requests.get(j["next"])
+                    j = r.json()
+                else:
+                    j = {}
+            for elem in retval:
+                print(elem)
+    else:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
