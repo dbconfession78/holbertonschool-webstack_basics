89c8f7bd5c1abbcd583631bda99bf2464d53fbcd
Stuart <sgkur04@gmail.com>
2018-06-06 04:25:14 +0000

    style update

--- 0x02-javascript_basics/102-concat.js
+++ 0x02-javascript_basics/102-concat.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const fs = require('fs');
-
 fs.readFile(args[0], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
+
   const a = data;
   fs.readFile(args[1], 'utf8', function (err, data) {
     if (err) {
