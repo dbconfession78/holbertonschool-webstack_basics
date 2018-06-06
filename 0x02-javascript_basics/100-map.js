f5f71583dc004062345f65e5759ee699c9b9ad7d
Stuart <sgkur04@gmail.com>
2018-06-06 03:24:57 +0000

    100-map.js :import list directly instead of whole file, then list

--- 0x02-javascript_basics/100-map.js
+++ 0x02-javascript_basics/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const data = require('./100-data.js');
-console.log(data.list);
+const list = require('./100-data.js').list;
+console.log(list);
 let n = [];
-for (let i = 0; i < data.list.length; i++) {
-  n.push(data.list[i] * i);
+for (let i = 0; i < list.length; i++) {
+  n.push(list[i] * i);
 }
 console.log(n);
