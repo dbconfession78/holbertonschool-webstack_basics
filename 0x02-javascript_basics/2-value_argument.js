83d79f970969b4ca687c56da2484975a9a4273c8
Stuart <sgkur04@gmail.com>
2018-06-12 23:56:16 +0000

    remove '.length'

--- 0x02-javascript_basics/2-value_argument.js
+++ 0x02-javascript_basics/2-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('No argument');
+const arg = process.argv.slice(2)[0];
+if (arg) {
+  console.log(arg);
 } else {
-  console.log(args[0]);
+  console.log('No argument');
 }
