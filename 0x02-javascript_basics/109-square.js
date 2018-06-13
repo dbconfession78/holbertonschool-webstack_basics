a0da6f9a768cacc2a10ec28451089b1fde9c36e5
Stuart <sgkur04@gmail.com>
2018-06-13 02:35:46 +0000

    add 'size' to Rectangle call as well

--- 0x02-javascript_basics/109-square.js
+++ 0x02-javascript_basics/109-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = require('./107-rectangle').Rectangle;
 function Square (size) {
-  Rectangle.call(this);
+  Rectangle.call(this, size);
   if (size > 0) {
     this.width = size;
     this.height = size;
