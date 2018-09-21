bddc9cb7b94c19c5e71a53eace1ba5ea97810c4b
Stuart Kuredjian <sgkur04@gmail.com>
2018-09-21 00:21:31 +0000

    move smicolon down one row

--- 0x05-sql_create_join/21-cities_of_california_subquery.sql
+++ 0x05-sql_create_join/21-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT cities.id, cities.name
 FROM cities
-WHERE state_id = 1;
-ORDER BY cities.id ASC
+WHERE state_id = 1
+ORDER BY cities.id ASC;
